da06371eadefdf7cff1b6b8601ed704300acf2ef
Vinicius Leite Batista <viniciusleite384@gmail.com>
2022-05-02 17:22:36 -0400

    use ReportCard to show search result

--- src/pages/search-results/index.js
+++ src/pages/search-results/index.js
@@ -1,10 +1,17 @@
 import { useSearchParams } from "react-router-dom";
+import { useState, useEffect } from "react";
+import reportsApi from "../../utils/reportsApi";
+import ReportCard from "../../components/report-card";
 
 const SearchResults = () => {
     const [searchParams] = useSearchParams();
     const searchResult = searchParams.get("search-result");
+    const [reports, setReports] = useState([]);
+    useEffect(() => {(async () => setReports(await reportsApi.getSearchResults(searchResult)))()}, [searchResult]);
     return (
-        <h1>{searchResult}</h1>
+        <div style={{padding: "1.53965vw 15.39646vw 0"}}>
+            {reports.map(report => <ReportCard key={report._id} { ...report } />)}
+        </div>
     );
 };
 

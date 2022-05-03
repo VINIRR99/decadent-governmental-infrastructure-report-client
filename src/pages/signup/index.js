c550e50159d53564e0b389c6b9a83eea670e5e99
Vinicius Leite Batista <viniciusleite384@gmail.com>
2022-05-03 10:28:00 -0400

    useState on signup

--- src/pages/signup/index.js
+++ src/pages/signup/index.js
@@ -1,29 +1,47 @@
+import { useState } from "react";
 import { Form, Title, Input, Button } from "./styles"
 
 const Signup = () => {
+    const [name, setName] = useState("");
+    const [username, setUsername] = useState("");
+    const [password, setPassword] = useState("");
+    const [passwordConfirmation, setPasswordConfirmation] = useState("");
     const inputs = [
         {
             type: "text",
-            placeholder: "Name"
+            placeholder: "Name",
+            value: name,
+            onChange: e => setName(e.target.value)
         },
         {
             type: "text",
-            placeholder: "Username"
+            placeholder: "Username",
+            value: username,
+            onChange: e => setUsername(e.target.value)
         },
         {
             type: "password",
-            placeholder: "Password"
+            placeholder: "Password",
+            value: password,
+            onChange: e => setPassword(e.target.value)
         },
         {
             type: "password",
-            placeholder: "Verify Password"
+            placeholder: "Verify Password",
+            value: passwordConfirmation,
+            onChange: e => setPasswordConfirmation(e.target.value)
         }
     ];
     return (
         <div>
             <Form>
                 <Title>Sign up</Title>
-                {inputs.map(input => <Input key={input.placeholder} type={input.type} placeholder={input.placeholder} />)}
+                {inputs.map(input => <Input key={input.placeholder}
+                    type={input.type}
+                    placeholder={input.placeholder}
+                    value={input.value}
+                    onChange={input.onChange}
+                />)}
                 <Button>Sign Up</Button>
             </Form>
         </div>

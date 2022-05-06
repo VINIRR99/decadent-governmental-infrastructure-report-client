b1320f1f90bb090265e09ffe2d52bd45d0d6813a
Vinicius Leite Batista <viniciusleite384@gmail.com>
2022-05-06 16:17:12 -0400

    logout button

--- src/components/dropdown-button/index.js
+++ src/components/dropdown-button/index.js
@@ -1,14 +1,43 @@
+import { useNavigate } from "react-router-dom";
+import { useState } from "react";
 import ProfileImage from "../ProfileImage";
 import { ArrowDownwardIcon } from "../Icons";
-import { Button } from "./styles";
+import { Button, DropdownList, DropdownContent } from "./styles";
 
-const DropdownButton = ({ profileImage }) => {
+const DropdownButton = ({ profileImage, username }) => {
+    const navigate = useNavigate();
+    const [showDropdown, setShowDropdown] = useState(false);
+
+    const handleProfile = () => {
+        setShowDropdown(false);
+        navigate(`/profile/${username}`);
+    };
+
+    const handleAccount = () => {
+        setShowDropdown(false);
+        navigate("/");
+    };
+
+    const handleLogout = () => {
+        localStorage.removeItem("token");
+        localStorage.removeItem("user");
+        navigate("/");
+        window.location.reload();
+    };
+
     return (
         <div>
-            <Button>
+            <Button onClick={() => setShowDropdown(!showDropdown)}>
                 <ProfileImage image={profileImage} size={30} margin="0 5px 0 0" />
                 <ArrowDownwardIcon />
             </Button>
+            {showDropdown && (
+                <DropdownList>
+                    <DropdownContent borderWidth="1px 1px 0" onClick={handleProfile}>Profile</DropdownContent>
+                    <DropdownContent borderWidth="0 1px 1px" onClick={handleAccount}>Account</DropdownContent>
+                    <DropdownContent borderWidth="0 1px 1px" onClick={handleLogout}>Logout</DropdownContent>
+                </DropdownList>
+            )}
         </div>
     );
 };
--- src/components/dropdown-button/styles.js
+++ src/components/dropdown-button/styles.js
@@ -8,3 +8,30 @@ export const Button = styled.button`
     border: 0;
     color: white;
 `;
+
+export const DropdownList = styled.ul`
+    list-style-type: none;
+    width: 0;
+    height: 0;
+    padding: 0;
+    margin: 0;
+    font-size: 1.5rem;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+`;
+
+export const DropdownContent = styled.li`
+    text-align: center;
+    width: 60px;
+    background-color: #dcdee5;
+    padding: 5px;
+    margin-right: -55px;
+    border: solid black;
+    cursor: pointer;
+    border-width: ${({ borderWidth }) => borderWidth};
+    &:hover {
+        background-color: #0d259b;
+        color: white;
+    }
+`;

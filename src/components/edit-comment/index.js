6d6bfce9ec384e5993e1ca85d2e1a0cb6d009d99
Vinicius Leite Batista <viniciusleite384@gmail.com>
2022-05-06 09:02:10 -0400

    buttons estilization

--- src/components/edit-comment/index.js
+++ src/components/edit-comment/index.js
@@ -1,8 +1,29 @@
-import { EditButton } from "./styles";
+import { useState } from "react";
+import { Div, EditButton, EditList, Buttons } from "./styles";
 
 const EditiComment = () => {
+    const [showOptions, setShowOptions] = useState(false);
+
+    const editComment = () => {
+        setShowOptions(false);
+    };
+
+    const deleteComment = () => {
+        setShowOptions(false);
+    };
+
     return (
-        <EditButton><strong>...</strong></EditButton>
+        <Div>
+            <EditButton condition={showOptions} onClick={() => setShowOptions(!showOptions)}>
+                <strong>...</strong>
+            </EditButton>
+            {showOptions && (
+                <EditList>
+                    <Buttons borderWidth="1px" onClick={editComment}>Edit</Buttons>
+                    <Buttons borderWidth="0 1px 1px" onClick={deleteComment}>Delete</Buttons>
+                </EditList>
+            )}
+        </Div>
     );
 };
 
--- src/components/edit-comment/styles.js
+++ src/components/edit-comment/styles.js
@@ -1,12 +1,40 @@
 import styled from "styled-components";
 
+export const Div = styled.div`
+`;
+
 export const EditButton = styled.button`
     height: fit-content;
     border: 0;
     border-radius: 40%;
-    background-color: #dcdee5;
+    background-color: ${({ condition }) => condition ? "gray" : "#dcdee5"};
     font-size: 1.5rem;
+    cursor: pointer;
     &:hover {
-        background-color: gray;
+        background-color: #bac2db;
+    }
+`;
+
+export const EditList = styled.ul`
+    list-style-type: none;
+    width: 0;
+    height: 0;
+    padding: 0;
+    font-size: 1.1rem;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+`;
+
+export const Buttons = styled.li`
+    text-align: center;
+    width: 50px;
+    background-color: #dcdee5;
+    padding: 5px;
+    border: solid black;
+    cursor: pointer;
+    border-width: ${({ borderWidth }) => borderWidth};
+    &:hover {
+        background-color: #bac2db;
     }
 `;

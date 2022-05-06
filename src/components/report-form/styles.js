adc83653d692c851cbc677fea38343919a3d5f72
Vinicius Leite Batista <viniciusleite384@gmail.com>
2022-05-05 22:52:27 -0400

    form component to use for report

---
+++ src/components/report-form/styles.js
@@ -0,0 +1,85 @@
+import styled from "styled-components";
+
+export const MainDiv = styled.div`
+    display: flex;
+    justify-content: center;
+    font-size: 1.5rem;
+`;
+
+export const File = styled.input`
+    color: transparent;
+    margin-bottom: 10px;
+    &::-webkit-file-upload-button {
+        visibility: hidden;
+    };
+    &:before {
+        content: "Select the image for the report";
+        color: black;
+        display: inline-block;
+        background: -webkit-linear-gradient(top, #f9f9f9, #e3e3e3);
+        border: 1px solid #999;
+        border-radius: 3px;
+        padding: 5px 8px;
+        outline: none;
+        white-space: nowrap;
+        cursor: pointer;
+        text-shadow: 1px 1px #fff;
+        font-weight: 700;
+        font-size: 10pt;
+    };
+    &:hover::before {
+        border-color: black;
+    };
+    &:active {
+        outline: 0;
+    };
+    &:active::before {
+        background: -webkit-linear-gradient(top, #e3e3e3, #f9f9f9); 
+    };
+`;
+
+export const MissImg = styled.p`
+    margin: 0;
+    color: red;
+`;
+
+export const Input = styled.input`
+    border-radius: 5px;
+    padding: 5px;
+    margin-bottom: 10px;
+    ::placeholder {
+        color: ${({ placeholderColor }) => placeholderColor};
+        opacity: 1;
+    }
+
+    :-ms-input-placeholder {
+        color: ${({ placeholderColor }) => placeholderColor};
+    }
+
+    ::-ms-input-placeholder {
+        color: ${({ placeholderColor }) => placeholderColor};
+    }
+`;
+
+export const Textarea = styled.textarea`
+    border-radius: 5px;
+    margin-bottom: 10px;
+`;
+
+export const Button = styled.button`
+    padding: 10px;
+    border-radius: 8px;
+    cursor: pointer;
+    background-color: #24252A;
+    color: white;
+`;
+
+export const Form = styled.form`
+    display: flex;
+    flex-direction: column;
+`;
+
+export const SubmitButton = styled(Button)`
+    background-color: green;
+    margin-right: 10px;
+`;
--- src/pages/report/index.js
+++ src/pages/report/index.js
@@ -1,6 +1,7 @@
 import { useParams } from "react-router-dom";
 import { useState, useEffect } from "react";
 import reportsApi from "../../utils/reportsApi";
+import ReportForm from "../../components/report-form";
 import ReportCard from "../../components/report-card";
 
 const Report = ({ loggedUser }) => {
@@ -11,6 +12,7 @@ const Report = ({ loggedUser }) => {
 
     return (Object.keys(report).length > 0) && (
         <div style={{padding: "1.53965vw 15.39646vw 0"}}>
+            {(loggedUser && (loggedUser._id === report.user._id)) && <ReportForm />}
             <ReportCard key={report._id} { ...report } loggedUser={loggedUser} />
         </div>
     );

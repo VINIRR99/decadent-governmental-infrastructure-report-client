40d4b4a30a3590d77ad169c1f2468cc163dd945d
Vinicius Leite Batista <viniciusleite384@gmail.com>
2022-05-07 07:35:41 -0400

    update profileImage

--- src/pages/account/index.js
+++ src/pages/account/index.js
@@ -1,10 +1,47 @@
+import { useState, useEffect } from "react";
 import ProfileImage from "../../components/ProfileImage";
+import { Div, Content, Button, File } from "./styles";
+import reportsApi from "../../utils/reportsApi";
 
 const Account = ({ loggedUser, setLoggedUser }) => {
-    return (
-        <div>
-            <ProfileImage image={loggedUser.profileImage} size={60} margin="0" />
-        </div>
+    const [user, setUser] = useState();
+
+    const [changeImage, setChangeImage] = useState(false);
+    const [imageFile, setImageFile] = useState();
+
+    useEffect(() => {
+        setUser(loggedUser);
+    }, [loggedUser]);
+
+    const cancelImageChange = () => {
+        setChangeImage(false);
+        setImageFile();
+    };
+
+    const submitImage = async () => {
+        const updatedUser = await reportsApi.uploadProfileImage(imageFile);
+        setUser(await updatedUser);
+        setLoggedUser(await updatedUser);
+        localStorage.removeItem("user");
+        localStorage.setItem("user", JSON.stringify(updatedUser));
+
+        cancelImageChange();
+    };
+
+    return user && (
+        <Div>
+            <Content>
+                {(user && user.profileImage) && <ProfileImage image={user.profileImage} size={350} margin="0 0 20px" />}
+                {changeImage && <File type="file" onChange={e => setImageFile(e.target.files[0])} accept="image/*" />}
+                {!changeImage && <Button onClick={() => setChangeImage(true)}>Edit profileImage</Button>}
+                {changeImage && (
+                    <div>
+                        <Button onClick={submitImage}>Submit image</Button>
+                        <Button onClick={cancelImageChange}>Cancel</Button>
+                    </div>
+                )}
+            </Content>
+        </Div>
     );
 };
 
---
+++ src/pages/account/styles.js
@@ -0,0 +1,58 @@
+import styled from "styled-components";
+
+export const Div = styled.div`
+    background-color: red;
+    display: flex;
+    justify-content: center;
+`;
+
+export const Content = styled.div`
+    background-color: blue;
+    width: fit-content;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    margin-top: 50px;
+`;
+
+export const Button = styled.button`
+    padding: 10px;
+    border-radius: 8px;
+    cursor: pointer;
+    background-color: #24252A;
+    color: white;
+    width: fit-content;
+`;
+
+export const File = styled.input`
+    color: transparent;
+    margin-bottom: 10px;
+    &::-webkit-file-upload-button {
+        visibility: hidden;
+    };
+    &:before {
+        content: "Select profile Image";
+        color: black;
+        display: inline-block;
+        background: -webkit-linear-gradient(top, #f9f9f9, #e3e3e3);
+        border: 1px solid #999;
+        border-radius: 3px;
+        padding: 5px 8px;
+        outline: none;
+        white-space: nowrap;
+        cursor: pointer;
+        text-shadow: 1px 1px #fff;
+        font-weight: 700;
+        font-size: 10pt;
+    };
+    &:hover::before {
+        border-color: black;
+    };
+    &:active {
+        outline: 0;
+    };
+    &:active::before {
+        background: -webkit-linear-gradient(top, #e3e3e3, #f9f9f9); 
+    };
+`;
